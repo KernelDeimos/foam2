2b88a281ef61a9227ba51c8cadf37a4e87d762fe
Eric <eric@nanopay.net>
2020-02-03 14:18:37 -0500

    PR-PENDING to not include google/foam/demos in foam-bin.js

--- src/foamlink.js
+++ src/foamlink.js
@@ -7,7 +7,7 @@ IGNORE('com/google/foam/demos/tabata/main.js');
 IGNORE('com/google/foam/experimental/Promise_test.js');
 
 // these files depend on global objects and should not be loaded by foamlink
-IGNORE('com/google/foam/demos/u2');
+IGNORE('com/google/foam/demos');
 
 // Tests should not be included by Foamlink
 IGNORE('test');
